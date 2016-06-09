044dd1efa0bc384804b0b863fc0222ec6896ce9a
Florin Iordache <iordache@adobe.com>
2016-06-09 15:45:26 +0300

    Detect and log change commits to the private stores
    * Using only a MountInfoProvider when building the validator

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/commit/PrivateStoreValidatorProvider.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/commit/PrivateStoreValidatorProvider.java
@@ -18,10 +18,7 @@
 package org.apache.jackrabbit.oak.plugins.commit;
 
 import org.apache.jackrabbit.oak.api.CommitFailedException;
-import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Tree;
-import org.apache.jackrabbit.oak.api.Type;
-import org.apache.jackrabbit.oak.plugins.multiplex.SimpleMountInfoProvider;
 import org.apache.jackrabbit.oak.plugins.tree.TreeFactory;
 import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
 import org.apache.jackrabbit.oak.spi.commit.DefaultValidator;
@@ -29,77 +26,35 @@ import org.apache.jackrabbit.oak.spi.commit.Validator;
 import org.apache.jackrabbit.oak.spi.commit.ValidatorProvider;
 import org.apache.jackrabbit.oak.spi.mount.Mount;
 import org.apache.jackrabbit.oak.spi.mount.MountInfoProvider;
-import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
-import org.apache.jackrabbit.oak.spi.whiteboard.Tracker;
-import org.apache.jackrabbit.oak.spi.whiteboard.Whiteboard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * {@link Validator} which detects change commits to the read only mounts.
  */
 public class PrivateStoreValidatorProvider extends ValidatorProvider {
 
-    private Whiteboard whiteboard;
-    private List<String> readOnlyRoots;
+    private MountInfoProvider mountInfoProvider;
+    private boolean failOnReadonlyMountCommit;
 
     /**
-     * Use a {@link Whiteboard} to determine the system's read only mounts
+     * Use a {@link MountInfoProvider} to determine the system's read only mounts
      *
-     * @param whiteboard - the {@link Whiteboard}
+     * @param mountInfoProvider - the {@link MountInfoProvider}
+     * @param failOnReadonlyMountCommit - fail when detecting a commit on a read only mount
+     *  or just log the commit information and let the commit go through
      */
-    public PrivateStoreValidatorProvider(Whiteboard whiteboard) {
-        this.whiteboard = whiteboard;
-    }
-
-    /**
-     * Use a fixed set of read only root paths
-     *
-     * @param readOnlyRoots - a list of read only root paths
-     */
-    public PrivateStoreValidatorProvider(String... readOnlyRoots) {
-        this.readOnlyRoots = Arrays.asList(readOnlyRoots);
+    public PrivateStoreValidatorProvider(MountInfoProvider mountInfoProvider, boolean failOnReadonlyMountCommit) {
+        this.mountInfoProvider = mountInfoProvider;
+        this.failOnReadonlyMountCommit = failOnReadonlyMountCommit;
     }
 
     @Nonnull
     public Validator getRootValidator(NodeState before, NodeState after, CommitInfo info) {
         return new PrivateStoreValidator();
-    }
-
-    /**
-     * Gets a {@link MountInfoProvider} service.
-     *
-     * @return a {@link MountInfoProvider} based on the fixed set of read only root paths, if any.
-     * Otherwise the {@link Whiteboard} will be used to attempt the resolution of a {@link MountInfoProvider}
-     * If all other methods fail, the {@code MountInfoProvider.DEFAULT} will be returned as a default.
-     */
-    private MountInfoProvider getMountInfoProvider() {
-        MountInfoProvider mountInfoProvider = MountInfoProvider.DEFAULT;
-
-        if (readOnlyRoots != null
-                && readOnlyRoots.size() > 0) {
-
-            mountInfoProvider = SimpleMountInfoProvider.newBuilder()
-                .readOnlyMount("readonly", readOnlyRoots.toArray(new String[0]))
-                .build();
-        } else if (whiteboard != null) {
-            Tracker<MountInfoProvider> mountInfoProviderTracker = whiteboard.track(MountInfoProvider.class);
-            List<MountInfoProvider> mountInfoProviderList = mountInfoProviderTracker.getServices();
-
-            if (mountInfoProviderList.size() > 0) {
-                mountInfoProvider = mountInfoProviderList.get(0);
-            }
-
-            mountInfoProviderTracker.stop();
-        }
-
-
-        return mountInfoProvider;
     }
 
     private class PrivateStoreValidator extends DefaultValidator {
@@ -109,32 +64,36 @@ public class PrivateStoreValidatorProvider extends ValidatorProvider {
         }
 
         public Validator childNodeAdded(String name, NodeState after) throws CommitFailedException {
-            logReadOnlyMountCommit(name, after);
-            return null;
+            return logReadOnlyMountCommit(name, after);
         }
 
         public Validator childNodeChanged(String name, NodeState before, NodeState after) throws CommitFailedException {
-            logReadOnlyMountCommit(name, after);
-            return null;
+            return logReadOnlyMountCommit(name, after);
         }
 
         public Validator childNodeDeleted(String name, NodeState before) throws CommitFailedException {
-            logReadOnlyMountCommit(name, before);
-            return null;
+            return logReadOnlyMountCommit(name, before);
         }
 
-        private void logReadOnlyMountCommit(String nodeName, NodeState nodeState) {
+        private Validator logReadOnlyMountCommit(String nodeName, NodeState nodeState) throws CommitFailedException {
             Tree nodeStateTree = TreeFactory.createReadOnlyTree(nodeState);
             String nodePath = nodeStateTree.getPath();
             String changeNodePath = nodeStateTree.isRoot()
                     ? nodePath + nodeName
                     : nodePath + "/" + nodeName;
 
-            MountInfoProvider mountInfoProvider = getMountInfoProvider();
             Mount mountInfo = mountInfoProvider.getMountByPath(changeNodePath);
             if (mountInfo.isReadOnly()) {
-                logger.error("Detected commit to a read-only store! ", new Throwable("Commit path: " + changeNodePath));
+                Throwable throwable = new Throwable("Commit path: " + changeNodePath);
+                logger.error("Detected commit to a read-only store! ", throwable);
+
+                if (failOnReadonlyMountCommit) {
+                    throw new CommitFailedException(CommitFailedException.UNSUPPORTED, 0,
+                            "Unsupported commit to a read-only store!", throwable);
+                }
             }
+
+            return null;
         }
     }
 }
